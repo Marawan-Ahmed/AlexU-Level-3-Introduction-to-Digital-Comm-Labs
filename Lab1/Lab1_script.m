ee9a1554afdb1029630688bc9ec01a22ca02618e
Robo-Tech <78983720+AmrRamadan01@users.noreply.github.com>
2023-02-24 22:45:03 +0200

    doc: add "[DONE]" comments to implemented tasks

--- Lab1/Lab1_script.m
+++ Lab1/Lab1_script.m
@@ -15,16 +15,16 @@ p      = 0.2;   % Channel parameter (probability of bit flipping)
 %% Part 1: BER for simple BSC channel
 
 % Generate a bit sequence
-bit_seq = GenerateBits(N_bits); % IMPLEMENT THIS: Generate a sequence of bits equal to the total number of bits
+bit_seq = GenerateBits(N_bits); % IMPLEMENT THIS [DONE]: Generate a sequence of bits equal to the total number of bits
 
 % Pass the bit sequence through the channel
 rec_sample_seq = BSC(bit_seq,1,p);  % Generate the received samples after passing through the bit flipping channel
 
 % Decode bits from received bit sequence
-rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_1');  % IMPLEMENT THIS: Decode the received bits
+rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_1');  % IMPLEMENT THIS [DONE]: Decode the received bits
 
 % Compute the BER
-BER_case_1 = ComputeBER(bit_seq,rec_bit_seq); % IMPLEMENT THIS: Calculate the bit error rate
+BER_case_1 = ComputeBER(bit_seq,rec_bit_seq); % IMPLEMENT THIS [DONE]: Calculate the bit error rate
 
 %% Part 1-a: Effect of bit flipping probability on BER
 % GOAL: Make a plot for the BER versus different values of the channel
@@ -33,7 +33,7 @@ BER_case_1 = ComputeBER(bit_seq,rec_bit_seq); % IMPLEMENT THIS: Calculate the bi
 p_vect          = 0:0.1:1;              % Use this vector to extract different values of p in your code
 BER_case_1_vec  = zeros(size(p_vect));  % Use this vector to store the resultant BER
 
-%%% WRITE YOUR CODE HERE
+%%% WRITE YOUR CODE HERE [DONE]
 for p_ind = 1:length(p_vect)
     rec_sample_seq = BSC(bit_seq,1,p_vect(p_ind));
     rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_1');
@@ -53,13 +53,13 @@ fs  = 10;    % Number of samples per symbol (bit)
 bit_seq = GenerateBits(N_bits); % Generate a sequence of bits equal to the total number of bits
 
 % Generate samples from bits
-sample_seq = GenerateSamples(bit_seq,fs); % IMPLEMENT THIS: Generate a sequence of samples for each bit
+sample_seq = GenerateSamples(bit_seq,fs); % IMPLEMENT THIS [DONE]: Generate a sequence of samples for each bit
 
 % Pass the sample sequence through the channel
 rec_sample_seq = BSC(sample_seq,fs,p);   % Generate the received samples after passing through the bit flipping channel
 
 % Decode bits from received bit sequence
-rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_2',fs);    % IMPLEMENT THIS: Decode the received bits
+rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_2',fs);    % IMPLEMENT THIS [DONE]: Decode the received bits
 
 % Compute the BER
 BER_case_2 = ComputeBER(bit_seq,rec_bit_seq);   % Calculate the bit error rate
@@ -71,7 +71,7 @@ BER_case_2 = ComputeBER(bit_seq,rec_bit_seq);   % Calculate the bit error rate
 p_vect          = 0:0.1:1;              % Use this vector to extract different values of p in your code
 BER_case_2_vec  = zeros(size(p_vect));  % Use this vector to store the resultant BER
 
-%%% WRITE YOUR CODE HERE
+%%% WRITE YOUR CODE HERE [DONE]
 for p_ind = 1:length(p_vect)
     rec_sample_seq = BSC(sample_seq,fs,p_vect(p_ind));
     rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_2',fs);
@@ -91,7 +91,7 @@ sample_seq = GenerateSamples(bit_seq,fs); % Generate a sequence of samples for e
 rec_sample_seq  = BSC(sample_seq,fs,p,'correlated'); % Generate the received samples after passing through the bit flipping channel
 
 % Decode bits from received bit sequence
-rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_3',fs);    % IMPLEMENT THIS: Decode the received bits
+rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,'part_3',fs);    % IMPLEMENT THIS [DONE]: Decode the received bits
 
 % Compute the BER
 BER_case_3 = ComputeBER(bit_seq,rec_bit_seq);  % Calculate the bit error rate
