0e1f1474e5a5790b2a7a1ed9fe484def36bfa869
Marawan-Ahmed <memoahmedrabee@yahoo.com>
2023-05-13 20:38:47 +0300

    probably done

--- project/Part1.m
+++ project/Part1.m
@@ -1,17 +1,20 @@
+close all;
+clear;
+
 % Simulation parameters
-n = 1e3; % number of bits
+n = 1e4; % number of bits (need to be changed before submit to 1e5)
 SNR = 0:2:30; % signal-to-noise ratio range (in dB)
 m = 20; % number of samples that represents waveform
 T = 20; % sampling instant
 s1 = ones(1,m); % rectangular signal with amplitude 1
 s2 = zeros(1,m); % zero signal
+matched_filter = (s1 - s2); % The fliplr function is used to flip the waveform so that it can be used as a filter.
+matched_filter = matched_filter(end:-1:1); % The fliplr function is used to flip the waveform so that it can be used as a filter.
 
 % Generate random binary data vector
-data = randi([0 1],1,n);
+data = randi([0 1],1,n) > 0.5;
 
 % Represent each bit with proper waveform
-% waveform = repmat(data,m,1);
-% waveform = reshape(waveform',[],1);
 waveform = [];
 for i = 1:length(data)
     if data(i) == 0
@@ -23,59 +26,84 @@ end
 
 % Apply noise to samples
 for i = 1:length(SNR)
-    snr = SNR(i);
-    noise_power = sum(data.^2)/10^(snr/10);
-    noise = sqrt(noise_power)*randn(size(waveform));
-    Rx_signal = waveform + noise;
+    noise_power = sum(data.^2)/10^(SNR(i)/10);
+    noise_signal = sqrt(noise_power) * randn(size(waveform));
+    Rx_signal = waveform + noise_signal;
+%     Rx_signal = waveform;
     
     % Decide whether the Rx_sequence is '1' or '0' by comparing the samples with threshold
-    threshold = sum(s1)/2;    
-    simple_Rx = simple_receiver(Rx_signal,T);
-    
+    simple_Rx = simple_receiver(Rx_signal,m,mean(Rx_signal),T-1);
     % Perform convolution process in the receiver
+    y = filter(matched_filter,1,Rx_signal);
+%     y = conv(Rx_signal,matched_filter(end:-1:1));
 
+    % Sample the output of the matched filter;
+    mf_Rx = simple_receiver(y,m,mean(y),T-1);
     
-    % Sample the output of the matched filter
-
-    
+    collerator_Rx = collerator_receiver(Rx_signal,m,s1,s2,T-1);
     % Compare the original bits with the detected bits and calculate number of errors
-    num_errors(i) = ComputeBER(data,simple_Rx);
-end
+    BER_simple_Rx(i) = ComputeBER(data,simple_Rx);
+    BER_mf(i) = ComputeBER(data,mf_Rx);
+    BER_collerator(i) = ComputeBER(data,collerator_Rx);
 
-% Save the probability of error of each SNR in matrix, BER
-BER = num_errors/n;
+end
 
 % Plot the BER curve against SNR (use semilogy)
-semilogy(SNR,BER,'-o')
+semilogy(SNR,BER_simple_Rx,'-o')
+xlim([0 SNR(end)])
+ylim([0 0.5])
 xlabel('Signal-to-Noise Ratio (dB)')
 ylabel('Bit Error Rate')
-title('Bit Error Rate vs. Signal-to-Noise Ratio')
+title('Matched filter vs. simple Rx')
+hold on
 
+semilogy(SNR,BER_mf,'-o')
 
+hold off
+figure;
+% Plot the BER curve against SNR (use semilogy)
+semilogy(SNR,BER_simple_Rx,'-o')
+xlim([0 SNR(end)])
+ylim([0 0.5])
+xlabel('Signal-to-Noise Ratio (dB)')
+ylabel('Bit Error Rate')
+title('Collerator vs. simple Rx')
+hold on
 
+semilogy(SNR,BER_collerator,'-o')
+
+hold off
 function BER = ComputeBER(bit_seq,rec_bit_seq)
-    number_error_bits = 0;
-    number_bits = length(bit_seq);
-
-    for i = 1:number_bits
-        if (bit_seq(i) ~= rec_bit_seq(i))
-            number_error_bits = number_error_bits + 1;
-        end
-    end
-    BER = number_error_bits / number_bits;
+    % Use bitxor to compute the bit-wise XOR of x and y
+    BER= sum(bitxor(bit_seq,rec_bit_seq)) / length(bit_seq);
 end
 
-function rec_bit_seq = simple_receiver(rec_sample_seq,T)
-    number_bits = length(rec_sample_seq)/T;
+function rec_bit_seq = simple_receiver(rec_sample_seq,m,v_th,T)
+    number_bits = length(rec_sample_seq)/m;
     rec_bit_seq = zeros(1 ,number_bits);
 
     for i = 1:number_bits
-        temp = 0;
-        for j = 1:T
-           temp = temp + rec_sample_seq((i-1)*T+j);
-        end
-        if temp >= T/2
+        if rec_sample_seq((i-1)*m+ T +1) >= v_th
             rec_bit_seq(i) = 1;
         end
     end
+end
+function rec_bit_seq = collerator_receiver(rec_sample_seq,m,s1,s2,T)
+    number_bits = length(rec_sample_seq)/m;
+    for i = 1:number_bits
+        temp = rec_sample_seq((i-1)*m+1:i*m);
+        temp = temp.*(s1-s2);
+        temp = sum(temp);
+        rec_bit_seq(i) = temp;
+    end
+    
+    v_th = mean(rec_bit_seq);
+    rec_bit_seq = rec_bit_seq > v_th;
+%     for i = 1:number_bits
+%         if rec_sample_seq(i) >= v_th
+%             rec_bit_seq(i) = 1;
+%         else
+%             rec_bit_seq(i) = 0;
+%         end 
+%     end
 end
