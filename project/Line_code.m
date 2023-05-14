df5b9c9af5fe15ed155f3bb7cae6b9ff99daa4cf
aliemadabdo <90727865+aliemadabdo@users.noreply.github.com>
2023-05-14 08:18:23 +0300

    Fix 3 functions: RZ, manchester and mlt3

--- project/Line_code.m
+++ project/Line_code.m
@@ -1,7 +1,43 @@
+%% Setup
 
 % Generate random bits
-num_bits = 100;
+num_bits = 1000;
+
+% Set the pulse duration
+pulse_duration = 1;  % Duration of each pulse in seconds
+
+% set redundancy to display abrupt changes 
+redundancy = 100;
+
+% Display the bit sequence
 bits = generate_random_bits(num_bits);
+%bits = [0,1,0,1,0,1,0,1,0,1];
+%disp ("generated bit sequence:"); disp(bits);
+
+% Generate the time vector
+num_samples = length(bits) * redundancy;
+time = linspace(0, pulse_duration * length(bits), num_samples); 
+
+% Sampling time
+Ts = length(time) / num_samples ; 
+
+% Plot the random generated bit sequence
+% Generate the voltage vector like abrupt changes
+bits_volt = repelem(bits, redundancy); 
+% About repelem(A,B) function
+% A: is the input array to be repeated
+% B: is the number of times each element in A should be repeated.
+
+%figure;
+%plot(time, bits_volt);
+%xlabel('Time');
+%ylabel('Voltage');
+%title('The random generated bit sequence');
+%ylim([-0.5, 1.5]);  % Set y-axis limits to show only 0 and 1 values
+
+%saveas(gcf, 'input_bits.png');
+
+%% Line codes Modulation
 
 % Modulate the bits using different line codes
 waveform_nrz = nrz_modulation(bits);
@@ -10,69 +46,47 @@ waveform_rz = rz_modulation(bits);
 waveform_ami = ami_modulation(bits);
 waveform_manchester = manchester_modulation(bits);
 waveform_mlt3 = mlt3_modulation(bits);
-
-
-% Plot the line code modulations
-time = (0 : num_bits-1) * 2;  % Assuming each bit lasts for 2 units of time
-
+ %
+% Plot different line codes modulation
 figure;
-stem(time, bits, 'filled', 'MarkerSize', 5);
-xlabel('Time');
-ylabel('Voltage');
-title('The random generated bit sequence');
-ylim([-0.5, 1.5]);  % Set y-axis limits to show only 0 and 1 values
 
-figure;
+waveform_nrz_volt = repelem(waveform_nrz, redundancy); 
 subplot(6, 1, 1);
-plot(time, waveform_nrz);
-xlabel('Time');
-ylabel('Voltage');
+plot(time, waveform_nrz_volt);
 title('NRZ Modulation');
 
+waveform_nrzi_volt = repelem(waveform_nrzi, redundancy); 
 subplot(6, 1, 2);
-plot(time, waveform_nrzi);
-xlabel('Time');
-ylabel('Voltage');
+plot(time, waveform_nrzi_volt);
 title('NRZI Modulation');
 
-resampled_waveform_rz = interp1(linspace(0, 200, 200), waveform_rz, time);
+waveform_rz_volt = repelem(waveform_rz, redundancy/2); % 50 repetitions to fit with time vector
 subplot(6, 1, 3);
-plot(time, resampled_waveform_rz);
-xlabel('Time');
-ylabel('Voltage');
+plot(time, waveform_rz_volt);
 title('RZ Modulation');
 
-subplot(6, 1, 4);
-plot(time, waveform_ami);
-xlabel('Time');
 ylabel('Voltage');
+
+waveform_ami_volt = repelem(waveform_ami, redundancy); 
+subplot(6, 1, 4);
+plot(time, waveform_ami_volt);
 title('AMI Modulation');
 
-resampled_waveform_manchester = interp1(linspace(0, 200, 200), waveform_manchester, time);
+waveform_manchester_voltage = repelem(waveform_manchester, redundancy/2); % 50 repetitions to fit with time vector
 subplot(6, 1, 5);
-plot(time, resampled_waveform_manchester);
-xlabel('Time');
-ylabel('Voltage');
+plot(time, waveform_manchester_voltage);
 title('Manchester Modulation');
 
+waveform_mlt3_volt = repelem(waveform_mlt3, redundancy);
 subplot(6, 1, 6);
-plot(time, waveform_mlt3);
-xlabel('Time');
-ylabel('Voltage');
+plot(time, waveform_mlt3_volt);
 title('MLT-3 Modulation');
 
+xlabel('Time');
 
-%axis off;
-
-% Adjust the spacing between subplots
-%subplot_spacing = 0.04;
-%subplot_position = get(gcf, 'Position');
-%subplot_position(4) = subplot_position(4) - subplot_spacing;
-%set(gcf, 'Position', subplot_position);
-
-Ts = length(time) / num_bits ;
-
-% Calculate power spectral density
+saveas(gcf, 'Modulations.png');
+ %}
+%% Calculate power spectral density
 [P_nrz, f_nrz] = periodogram(waveform_nrz,[],[],1/Ts,'centered');
 [P_nrzi, f_nrzi] = periodogram(waveform_nrzi,[],[],1/Ts,'centered');
 [P_rz, f_rz] = periodogram(waveform_rz,[],[],1/Ts,'centered');
@@ -82,15 +96,38 @@ Ts = length(time) / num_bits ;
 
 % Plot power spectral density
 figure;
-subplot(6,1,1); plot(f_nrz,10*log10(P_nrz)); title('Non-return to zero');
-subplot(6,1,2); plot(f_nrzi,10*log10(P_nrzi)); title('Non-return to zero inverted');
-subplot(6,1,3); plot(f_rz,10*log10(P_rz)); title('Return to zero');
-subplot(6,1,4); plot(f_ami,10*log10(P_ami)); title('Alternate mark inversion');
-subplot(6,1,5); plot(f_manchester,10*log10(P_manchester)); title('Manchester coding');
-subplot(6,1,6); plot(f_mlt3,10*log10(P_mlt3)); title('Multi-level transmission3');
-xlabel('Frequency (Hz)');
+
+subplot(6,1,1); 
+plot(f_nrz,10*log10(P_nrz)); 
+title('Non-return to zero');
+
+subplot(6,1,2); 
+plot(f_nrzi,10*log10(P_nrzi)); 
+title('Non-return to zero inverted');
+
+subplot(6,1,3); 
+plot(f_rz,10*log10(P_rz)); 
+title('Return to zero');
+
 ylabel('Power spectral density (dB/Hz)');
 
+subplot(6,1,4); 
+plot(f_ami,10*log10(P_ami)); 
+title('Alternate mark inversion');
+
+subplot(6,1,5); 
+plot(f_manchester,10*log10(P_manchester)); 
+title('Manchester coding');
+
+subplot(6,1,6); 
+plot(f_mlt3,10*log10(P_mlt3)); 
+title('Multi-level transmission3');
+
+xlabel('Frequency (Hz)');
+
+saveas(gcf, 'PSD.png');
+
+%% Functions
 % Function to generate random bits
 function bits = generate_random_bits(num_bits)
     bits = randi([0, 1], 1, num_bits);
@@ -111,8 +148,16 @@ end
 
 % Function for Return to Zero (RZ) modulation
 function waveform = rz_modulation(bits)
-    waveform = repelem(bits, 2);
-    waveform(waveform == 0) = -1;  % Map 0 to -1
+    waveform = zeros(1, length(bits) * 2);
+    for i = 1:length(bits)
+        if bits(i) == 1
+            waveform(2*i-1) = 1;
+            %waveform(2*i) = 0;
+        else
+            waveform(2*i-1) = -1;
+            %waveform(2*i) = 0;
+        end
+    end
 end
 
 % Function for Alternative Mark Inversion (AMI) modulation
@@ -131,7 +176,7 @@ end
 function waveform = manchester_modulation(bits)
     waveform = zeros(1, length(bits) * 2);
     for i = 1:length(bits)
-        if bits(i) == 0
+        if bits(i) == 1
             waveform(2*i-1) = 1;
             waveform(2*i) = -1;
         else
@@ -144,15 +189,36 @@ end
 % Function for Multilevel Transmission 3 (MLT-3) modulation
 function waveform = mlt3_modulation(bits)
     waveform = zeros(1, length(bits));
-    level = 1;  % Initial level
+    level = 0;  % Initial level
+    past_level = -1;
+
     for i = 1:length(bits)
         if bits(i) == 0
             waveform(i) = level;  % No transition, maintain current level
         else
-            level = -level;  % Transition, change level
-            waveform(i) = level;
+            if (level == 1)
+                waveform(i) = 0;
+                level = 0;
+                past_level = 1;
+
+            elseif (level == -1)
+                waveform(i) = 0;
+                level = 0;
+                past_level = -1;
+
+            elseif (level == 0)
+                if (past_level == -1)
+                    waveform(i) = 1;
+                    level = 1;
+
+                elseif (past_level == 1)
+                    waveform(i) = -1;
+                    level = -1;
+
+                end
+                past_level = 0;
+            end
         end
     end
 end
-
 
